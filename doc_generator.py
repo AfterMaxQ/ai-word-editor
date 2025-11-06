03c5652edc083e0fd0ecc66085e161b49e03dd25
AfterMaxQ <1455726631@qq.com>
2025-11-06 17:58:09 +0800

    Initial commit: Setup project structure

---
+++ doc_generator.py
@@ -0,0 +1,8 @@
+import json
+
+def main():
+    print("Generator script is running.")
+    # We will add the logic to read the JSON and create the docx here later.
+
+if __name__ == "__main__":
+    main()
